28401cf8cdc3a6e552e68741ed00323390d0ef6a
mpbnka <acharyamudumbi428@gmail.com>
2021-08-08 08:02:14 -0400

    initial steps to install tomcat

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default['tomcat']['tomcat_version']="8.5.20"
+default['tomcat']['install_dir']="/opt/tomcat"
+default['tomcat']['tomcat_url']="http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz"
+default['tomcat']['tomcat_user']="tomcat" 
+default['tomcat']['tomcat_auto_start']="true" 
+default['tomcat']['java_install_version'] = 'java-1.7.0-openjdk-devel'
+default['tomcat']['path_service_file'] = '/etc/systemd/system/tomcat.service'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,90 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+
+# install jdk
+yum_package ['java-1.8.0-openjdk-devel.x86_64', 'wget'] do
+	action :install
+end
+
+# Download tomcat
+tomcat_url = node['tomcat']['url']
+
+script "Download Tomcat" do
+	interpreter "bash"
+	cwd "/tmp"
+	code <<-EOH
+		curl "#{tomcat_url}" --output "/tmp/apache-tomcat.tar.gz"
+	EOH
+end
+
+# create tomcat user
+group "tomcat"
+user "tomcat" do
+	group "tomcat"
+	system true
+	shell "/bin/bash"
+end
+
+# create tomcat directory
+tomcat_install_dir = node['tomcat']['install_dir']
+directory "#{tomcat_install_dir}" do
+	owner 'tomcat'
+	group 'tomcat'
+	recursive true
+	mode '0755'
+	action :create
+end
+
+# extract tomcat to target directory
+
+script "Extract Tomcat" do
+	interpreter "bash"
+	cwd "/tmp"
+	code <<-EOH
+		tar -zxvf /tmp/apache-tomcat.tar.gz -C #{tomcat_install_dir} --strip-components=1
+	EOH
+end
+
+path_service_file = node['tomcat']['path_service_file']
+# copy service file to destination
+template "#{path_service_file}" do
+	source 'service.erb'
+	owner 'root'
+	group 'root'
+	mode '0755'
+end
+
+# Reload systemctl daemon
+script "Reload Systemctl daemon" do
+	interpreter "bash"
+	code <<-EOH
+		systemctl daemon-reload
+	EOH
+end
+
+
+# starttomcat to destination directory
+script "Move to destination directory" do
+	interpreter "bash"
+	code <<-EOH
+		#{tomcat_install_dir}/bin/startup.sh
+	EOH
+end
+
+# ensure tomcat	is started
+script "Start Tomcat" do
+	interpreter "bash"
+	code <<-EOH
+		systemctl start tomcat
+	EOH
+end
+
+#ensure tomcat is enabled
+script "Enable Tomcat" do
+	        interpreter "bash"
+        code <<-EOH
+                systemctl enable tomcat
+        EOH
+end
+
